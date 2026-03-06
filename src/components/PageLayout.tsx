50fd76af7b95d3687cad24d91defd73fbaec7bf2
stefan5441 <stefan.cambov2000@gmail.com>
2026-03-06 23:38:41 +0100

    Small bugfix, scroll appears and it shouldn\t

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -10,7 +10,7 @@ export const PageLayout = ({ children }: Props) => {
   return (
     <div className="flex h-screen flex-col px-8 xl:px-36">
       <NavBar />
-      <div className="my-8 min-h-0 w-full flex-1 overflow-y-auto xl:my-0">
+      <div className="min-h-0 w-full flex-1 overflow-y-auto py-8 xl:py-0">
         {children}
       </div>
       <div className="flex h-16 shrink-0 items-center justify-between">
